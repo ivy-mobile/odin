073c26b370724cfcce66094a778d894085ad29ec
zhaobin <357958137@qq.com>
2025-09-25 11:48:53 +0800

    style(xlog): TimeFieldFormat

--- xutil/xlog/xlog.go
+++ xutil/xlog/xlog.go
@@ -45,7 +45,8 @@ func Init(level, pathname string, interval time.Duration, serviceName, env strin
 	}
 
 	//zerolog.TimeFieldFormat = zerolog.TimeFormatUnix // 更快更小
-	//zerolog.TimeFieldFormat = "2006-01-02 15:04:05:000"
+	//zerolog.TimeFieldFormat = "2006-01-02 15:04:05" // 秒级,不带时区
+	zerolog.TimeFieldFormat = "2006-01-02T15:04:05.000Z700" // 毫秒级,带时区
 	//zerolog.TimestampFieldName = "timestamp"
 	//zerolog.LevelFieldName = "Level"
 	//zerolog.MessageFieldName = "msg"
