f0ccb70ef0d751a2e6026f4fe9a9f597087535ae
leo <leo>
2026-06-05 16:02:46 +0800

    feat(config): 新增node_group配置项支持
    - 在example.json配置文件中添加了node_group字段
    - 在example.toml配置文件中添加了node_group字段
    - 在example.yaml配置文件中添加了node_group字段
    - 在mq.go配置结构体中新增NodeGroup字段以支持该配置项

--- conf/mq.go
+++ conf/mq.go
@@ -5,6 +5,7 @@ type RocketMQConfig struct {
 	Endpoint      string `yaml:"endpoint" json:"endpoint" toml:"endpoint"`
 	Namespace     string `yaml:"namespace" json:"namespace" toml:"namespace"`
 	Group         string `yaml:"group" json:"group" toml:"group"`
+	NodeGroup     string `yaml:"node_group" json:"node_group" toml:"node_group"`
 	AccessKey     string `yaml:"access_key" json:"access_key" toml:"access_key"`
 	SecretKey     string `yaml:"secret_key" json:"secret_key" toml:"secret_key"`
 	SecurityToken string `yaml:"security_token" json:"security_token" toml:"security_token"`
